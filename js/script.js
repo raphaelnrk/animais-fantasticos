384b9018a35db56ecf64e74bf6c15382ae85a129
Raphael Pena <raphaelnrk@gmail.com>
2020-12-02 16:23:22 -0300

    ScrollSuave transformado em classe

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Usar sempre o 'use strict' para controle de dados.
 
-import initScrollSuave from "./modules/scroll-suave.js";
+import ScrollSuave from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabNav.js"
@@ -12,7 +12,8 @@ import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetchanimais.js";
 import initFetchBitcoin from "./modules/fetchbitcoin.js";
 
-initScrollSuave();
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
